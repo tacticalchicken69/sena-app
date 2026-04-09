a1fceae4c910153f8d000f9b73476a5ccff40167
Andres Gonzalez <andres_gonz4lez@outlook.com>
2026-04-09 11:43:09 -0500

    cambios aws

--- SENA_APP/SENA_APP/settings.py
+++ SENA_APP/SENA_APP/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(g445kimxwu8jtl##b!lbhg$eemk-j&twv7j_jxoojt!gar%zu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
