199b620d69265a3ca24479c95d3a08315c8c97ad
Andres Gonzalez <andres_gonz4lez@outlook.com>
2026-04-10 22:46:14 -0500

    despliegue senapp aws

--- SENA_APP/SENA_APP/settings.py
+++ SENA_APP/SENA_APP/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "bootstrap5",
+    "django_bootstrap5",
     "aprendices",
     "instructores",
     "programas",
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
